174cfebd2111829a1a094d4dcba383a724154273
joecole1011 <akawit_7@hotmail.com>
2021-05-14 00:29:39 +0700

    Panel
    Panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { ProductItemComponent } from './product-list/product-item/product-item.component';
-import { ProductListComponent } from './product-list/product-list.component'
+import { ProductListComponent } from './product-list/product-list.component';
+import { PanelComponent } from './panel/panel.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductItemComponent,
-    ProductListComponent
+    ProductListComponent,
+    PanelComponent
 
   ],
   imports: [BrowserModule],
